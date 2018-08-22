6736ec79d8a181c309b72fb38d65614e77261a49
Virgil Martinez <vam021@shsu.edu>
2018-08-21 21:20:17 -0500

    Initial Commit

---
+++ BJJ Buddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BJJ Buddy
+//
+//  Created by Virgil Martinez on 8/21/18.
+//  Copyright © 2018 Virgil Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
