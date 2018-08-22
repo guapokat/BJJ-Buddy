791523e688862af6b8981f5a4411d7dc7d803bed
Virgil Martinez <vam021@shsu.edu>
2018-08-21 23:54:21 -0500

    Worked on Tab Bar. Have the animation library figured out for it. Also have cocoa pods installed now.

---
+++ BJJ Buddy/Controllers/JournalViewController.swift
@@ -0,0 +1,19 @@
+//
+//  JournalViewController.swift
+//  BJJ Buddy
+//
+//  Created by Virgil Martinez on 8/21/18.
+//  Copyright © 2018 Virgil Martinez. All rights reserved.
+//
+
+import UIKit
+
+class JournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
